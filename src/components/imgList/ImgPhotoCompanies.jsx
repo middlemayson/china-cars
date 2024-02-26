74c581088994d35cee4e04e300ad6935586f9808
middlemayson <middle.mayson@gmail.com>
2024-02-26 13:35:54 +0600

    minor changes

--- src/components/imgList/ImgPhotoCompanies.jsx
+++ src/components/imgList/ImgPhotoCompanies.jsx
@@ -3,7 +3,7 @@ import imgPhotoByd from '../../img/photo/company/byd.jpg';
 import imgPhotoChangan from '../../img/photo/company/Changan.jpg';
 import imgPhotoChery from '../../img/photo/company/chery.jpg';
 import imgPhotoDfm from '../../img/photo/company/dongfeng.jpg';
-import imgPhotoFaw from './../../img/photo/company/faw.jpg';
+import imgPhotoFaw from './../../img/photo/company/faw_company.jpg';
 import imgPhotoGac from '../../img/photo/company/GAC Group.jpg';
 import imgPhotoGelly from '../../img/photo/company/geely.jpg';
 import imgPhotoGWM from '../../img/photo/company/gwm.jpg';
