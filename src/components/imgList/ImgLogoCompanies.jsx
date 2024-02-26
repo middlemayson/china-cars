80622513bc773e63c7fa21a3db86a39c7a6d3c62
middlemayson <middle.mayson@gmail.com>
2024-02-26 13:34:51 +0600

    minor changes

--- src/components/imgList/ImgLogoCompanies.jsx
+++ src/components/imgList/ImgLogoCompanies.jsx
@@ -1,33 +1,33 @@
-import imgLogoGelly from '../../img/logo/company/Geely.png';
-import imgLogoChery from '../../img/logo/company/Chery_new.png';
-import imgLogoJac from '../../img/logo/company/jac_group.png';
-import imgLogoGWM from '../../img/logo/company/gwm.png';
 import imgLogoBaic from '../../img/logo/company/BAIC.png';
 import imgLogoByd from '../../img/logo/company/BYD.png';
+import imgLogoChangan from '../../img/logo/company/Changan.png';
+import imgLogoChery from '../../img/logo/company/Chery_new.png';
 import imgLogoDfm from '../../img/logo/company/dfm.png';
 import imgLogoFaw from '../../img/logo/company/FAW_Group.png';
 import imgLogoGac from '../../img/logo/company/GAC_Group.png';
-import imgLogoZx from '../../img/logo/company/ZX_AUTO.png';
-import imgLogoYutong from '../../img/logo/company/Yutong.png';
-import imgLogoSaic from '../../img/logo/company/SAIC-Motor.png';
-import imgLogoChangan from '../../img/logo/company/Changan.png';
+import imgLogoGelly from '../../img/logo/company/Geely.png';
+import imgLogoGWM from '../../img/logo/company/gwm.png';
+import imgLogoJac from '../../img/logo/company/jac_group.png';
 import imgLogoLi from '../../img/logo/company/Li_Auto.png';
+import imgLogoSaic from '../../img/logo/company/SAIC-Motor.png';
+import imgLogoYutong from '../../img/logo/company/Yutong.png';
+import imgLogoZx from '../../img/logo/company/ZX_AUTO.png';
 
 const ImgLogoCompanies = {
-    imgLogoGelly,
-    imgLogoChery,
-    imgLogoJac,
-    imgLogoGWM,
     imgLogoBaic,
     imgLogoByd,
+    imgLogoChangan,
+    imgLogoChery,
     imgLogoDfm,
     imgLogoFaw,
     imgLogoGac,
-    imgLogoZx,
-    imgLogoYutong,
+    imgLogoGelly,
+    imgLogoGWM,
+    imgLogoJac,
+    imgLogoLi,
     imgLogoSaic,
-    imgLogoChangan,
-    imgLogoLi
+    imgLogoYutong,
+    imgLogoZx
 }
 
 export default ImgLogoCompanies
