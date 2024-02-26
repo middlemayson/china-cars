c5693f5e53bd24329e2a604a5e3f812c2b1ce60b
middlemayson <middle.mayson@gmail.com>
2024-02-26 13:27:15 +0600

    Sorted files

--- src/components/imgList/ImgLogoBrands.jsx
+++ src/components/imgList/ImgLogoBrands.jsx
@@ -1,27 +1,47 @@
 import imgLogoAeolus from '../../img/logo/brands/Aeolus_logo-removebg-preview.png';
+import imgLogoArcfox from '../../img/logo/brands/Arcfox-logo.png';
 import imgLogoAstonMartin from '../../img/logo/brands/AstonMartin_logo.png';
 import imgLogoAvatr from '../../img/logo/brands/Avatr_Technology_Logo.png';
 import imgLogoBaojun from '../../img/logo/brands/Baojun_Logo.png';
+import imgLogoBeijing from '../../img/logo/brands/Beijing-logo.png';
+import imgLogoBeijingBenz from '../../img/logo/brands/Beijing_Benz_Automotive_logo.png';
+import imgLogoByd from '../../img/logo/brands/BYD_Auto_logo.png';
+import imgLogoChanghe from '../../img/logo/brands/Changhe-logo.png';
 import imgLogoCheryJagLand from '../../img/logo/brands/CHERY-JAGUAR-LANDROVER_logo.png';
+import imgLogoCowin from '../../img/logo/brands/Kaiyi_logo.png';
 import imgLogoDeepal from '../../img/logo/brands/Deepal_Logo.png';
+import imgLogoDenza from '../../img/logo/brands/Denza_logo.png';
+import imgLogoExeed from '../../img/logo/brands/exeed-logo.png';
+import imgLogoFangchengbao from '../../img/logo/brands/Fangchengbao_logo.png';
 import imgLogoFarizon from '../../img/logo/brands/Farizon_logo.png';
 import imgLogoFawVw from '../../img/logo/brands/fawvw_logo.png';
 import imgLogoFord from '../../img/logo/brands/Ford_logo.png';
 import imgLogoForthing from '../../img/logo/brands/Forthing-Logo.png';
+import imgLogoFoton from '../../img/logo/brands/Foton_Motor_logo.png';
+import imgLogoGAC from '../../img/logo/brands/gac_logo.png';
+import imgLogoGACAion from '../../img/logo/brands/GAC_Aion_2022_logo.png';
+import imgLogoGACHino from '../../img/logo/brands/GAC_Hino_logo.png';
+import imgLogoGACHonda from '../../img/logo/brands/Guangqi_Honda_logo.png';
+import imgLogoGACToyota from '../../img/logo/brands/Guangzhou-Toyota_Markenlogo-removebg-preview.png';
 import imgLogoGeely from '../../img/logo/brands/Geely_Auto_logo.png';
 import imgLogoGeometry from '../../img/logo/brands/Geometry_logo.png';
 import imgLogoGm from '../../img/logo/brands/Saic-gm_logo.png';
+import imgLogoGw from '../../img/logo/brands/Great-Wall-Logo.png';
 import imgLogoGWMcar from '../../img/logo/brands/gwm_logo.png';
 import imgLogoHaval from '../../img/logo/brands/haval_logo.png';
 import imgLogoHonda from '../../img/logo/brands/DfHonda_logo.png';
 import imgLogoHongqi from '../../img/logo/brands/Hongqi-Logo.png';
 import imgLogoHongyan from '../../img/logo/brands/SAIC_Iveco_Hongyan_logo.png';
+import imgLogoHyundai from '../../img/logo/brands/Hyundai_logo.png';
 import imgLogoIm from '../../img/logo/brands/IM_Motors_logo.png';
+import imgLogoJac from '../../img/logo/brands/Jac-motors_logo.png';
+import imgLogoJaecoo from '../../img/logo/brands/Jaecoo_logo.png';
 import imgLogoJBestune from '../../img/logo/brands/bestune_logo.png';
 import imgLogoJetour from '../../img/logo/brands/Jetour_Logo.png';
 import imgLogoJidu from '../../img/logo/brands/Pixel-J_logo.png';
 import imgLogoJiefang from '../../img/logo/brands/FAW_Jiefang_logo.png';
 import imgLogoJunfeng from '../../img/logo/brands/junfeng.png';
+import imgLogoKarry from '../../img/logo/brands/Karry_Logo.png';
 import imgLogoLEVC from '../../img/logo/brands/london-ev-company-logo.png';
 import imgLogoLivan from '../../img/logo/brands/Livian_Automotive_Logo.png';
 import imgLogoLotus from '../../img/logo/brands/Lotus_Cars_logo.png';
@@ -33,11 +53,13 @@ import imgLogoMengshi from '../../img/logo/brands/Mengshi_logo.png';
 import imgLogoMG from '../../img/logo/brands/Mg_logo.png';
 import imgLogoNAC from '../../img/logo/brands/NAC_logo.png';
 import imgLogoNissan from '../../img/logo/brands/DF_nissan_logo.png';
+import imgLogoOmoda from '../../img/logo/brands/Omoda_logo.png';
 import imgLogoOra from '../../img/logo/brands/ora_Logo.png';
 import imgLogoOshan from '../../img/logo/brands/Oshan_logo.png';
 import imgLogoPeugeotCitroën from '../../img/logo/brands/Dongfeng_Peugeot-Citroën_logo.png';
 import imgLogoPolestar from '../../img/logo/brands/Polestar_Logo.png';
 import imgLogoProton from '../../img/logo/brands/Proton_Logo.png';
+import imgLogoQoros from '../../img/logo/brands/qoros-logo.png';
 import imgLogoRadar from '../../img/logo/brands/radar_logo.png';
 import imgLogoRenaultKorea from '../../img/logo/brands/Renault_Korea_Motors.png';
 import imgLogoRising from '../../img/logo/brands/Rising_Auto_logo.png';
@@ -46,62 +68,60 @@ import imgLogoSehol from '../../img/logo/brands/Sehol_logo.png';
 import imgLogoSmart from '../../img/logo/brands/Smart_logo.png';
 import imgLogoSunwin from '../../img/logo/brands/Sunwin_logo.png';
 import imgLogoTank from '../../img/logo/brands/tank_logo.png';
+import imgLogoTrumpchi from '../../img/logo/brands/Trumpchi_brand_logo.png';
 import imgLogoVenucia from '../../img/logo/brands/Venucia_logo.png';
 import imgLogoVolvo from '../../img/logo/brands/Volvo-logo.png';
 import imgLogoVoyah from '../../img/logo/brands/voyah-logo.png';
 import imgLogoVw from '../../img/logo/brands/SAIC_Volkswagen_logo.png';
 import imgLogoWey from '../../img/logo/brands/WEY_logo.png';
-import imgLogoZeekr from '../../img/logo/brands/Zeekr_logo.png';
-import imgLogoQoros from '../../img/logo/brands/qoros-logo.png';
-import imgLogoCowin from '../../img/logo/brands/Kaiyi_logo.png';
-import imgLogoExeed from '../../img/logo/brands/exeed-logo.png';
-import imgLogoKarry from '../../img/logo/brands/Karry_Logo.png';
-import imgLogoOmoda from '../../img/logo/brands/Omoda_logo.png';
-import imgLogoJaecoo from '../../img/logo/brands/Jaecoo_logo.png';
-import imgLogoJac from '../../img/logo/brands/Jac-motors_logo.png';
-import imgLogoGw from '../../img/logo/brands/Great-Wall-Logo.png';
-import imgLogoFoton from '../../img/logo/brands/Foton_Motor_logo.png';
-import imgLogoHyundai from '../../img/logo/brands/Hyundai_logo.png';
-import imgLogoBeijingBenz from '../../img/logo/brands/Beijing_Benz_Automotive_logo.png';
-import imgLogoBeijing from '../../img/logo/brands/Beijing-logo.png';
-import imgLogoArcfox from '../../img/logo/brands/Arcfox-logo.png';
-import imgLogoChanghe from '../../img/logo/brands/Changhe-logo.png';
 import imgLogoYangwang from '../../img/logo/brands/Yangwang_logo.png';
-import imgLogoFangchengbao from '../../img/logo/brands/Fangchengbao_logo.png';
-import imgLogoDenza from '../../img/logo/brands/Denza_logo.png';
-import imgLogoByd from '../../img/logo/brands/BYD_Auto_logo.png';
-import imgLogoGACHino from '../../img/logo/brands/GAC_Hino_logo.png';
-import imgLogoGACToyota from '../../img/logo/brands/Guangzhou-Toyota_Markenlogo-removebg-preview.png';
-import imgLogoGACHonda from '../../img/logo/brands/Guangqi_Honda_logo.png';
-import imgLogoTrumpchi from '../../img/logo/brands/Trumpchi_brand_logo.png';
-import imgLogoGAC from '../../img/logo/brands/gac_logo.png';
-import imgLogoGACAion from '../../img/logo/brands/GAC_Aion_2022_logo.png';
+import imgLogoZeekr from '../../img/logo/brands/Zeekr_logo.png';
 
 const ImgLogoBrands = {
     imgLogoAeolus,
+    imgLogoArcfox,
     imgLogoAstonMartin,
     imgLogoAvatr,
     imgLogoBaojun,
+    imgLogoBeijing,
+    imgLogoBeijingBenz,
+    imgLogoByd,
+    imgLogoChanghe,
     imgLogoCheryJagLand,
+    imgLogoCowin,
     imgLogoDeepal,
+    imgLogoDenza,
+    imgLogoExeed,
+    imgLogoFangchengbao,
     imgLogoFarizon,
     imgLogoFawVw,
     imgLogoFord,
     imgLogoForthing,
+    imgLogoFoton,
+    imgLogoGAC,
+    imgLogoGACAion,
+    imgLogoGACHino,
+    imgLogoGACHonda,
+    imgLogoGACToyota,
     imgLogoGeely,
     imgLogoGeometry,
     imgLogoGm,
+    imgLogoGw,
     imgLogoGWMcar,
     imgLogoHaval,
     imgLogoHonda,
     imgLogoHongqi,
     imgLogoHongyan,
+    imgLogoHyundai,
     imgLogoIm,
+    imgLogoJac,
+    imgLogoJaecoo,
     imgLogoJBestune,
     imgLogoJetour,
     imgLogoJidu,
     imgLogoJiefang,
     imgLogoJunfeng,
+    imgLogoKarry,
     imgLogoLEVC,
     imgLogoLivan,
     imgLogoLotus,
@@ -113,11 +133,13 @@ const ImgLogoBrands = {
     imgLogoMG,
     imgLogoNAC,
     imgLogoNissan,
+    imgLogoOmoda,
     imgLogoOra,
     imgLogoOshan,
     imgLogoPeugeotCitroën,
     imgLogoPolestar,
     imgLogoProton,
+    imgLogoQoros,
     imgLogoRadar,
     imgLogoRenaultKorea,
     imgLogoRising,
@@ -126,36 +148,14 @@ const ImgLogoBrands = {
     imgLogoSmart,
     imgLogoSunwin,
     imgLogoTank,
+    imgLogoTrumpchi,
     imgLogoVenucia,
     imgLogoVolvo,
     imgLogoVoyah,
     imgLogoVw,
     imgLogoWey,
-    imgLogoZeekr,
-    imgLogoQoros,
-    imgLogoCowin,
-    imgLogoExeed,
-    imgLogoKarry,
-    imgLogoOmoda,
-    imgLogoJaecoo,
-    imgLogoJac,
-    imgLogoGw,
-    imgLogoFoton,
-    imgLogoHyundai,
-    imgLogoBeijingBenz,
-    imgLogoBeijing,
-    imgLogoArcfox,
-    imgLogoChanghe,
     imgLogoYangwang,
-    imgLogoFangchengbao,
-    imgLogoDenza,
-    imgLogoByd,
-    imgLogoGACHino,
-    imgLogoGACToyota,
-    imgLogoGACHonda,
-    imgLogoTrumpchi,
-    imgLogoGAC,
-    imgLogoGACAion
+    imgLogoZeekr
 }
 
 export default ImgLogoBrands;
--- src/components/imgList/ImgPhotoCompanies.jsx
+++ src/components/imgList/ImgPhotoCompanies.jsx
@@ -1,33 +1,33 @@
-import imgPhotoGelly from '../../img/photo/company/geely.jpg';
-import imgPhotoChery from '../../img/photo/company/chery.jpg';
-import imgPhotoJac from '../../img/photo/company/jac.jpg';
-import imgPhotoGWM from '../../img/photo/company/gwm.jpg';
 import imgPhotoBaic from '../../img/photo/company/baic.jpg';
 import imgPhotoByd from '../../img/photo/company/byd.jpg';
-import imgPhotoDfm from '../../img/photo/company/dongfeng.jpg';
-import imgPhotoFaw from './../../img/photo/company/faw.jpg';
-import imgPhotoGac from '../../img/photo/company/GAC Group.jpg';
-import imgPhotoZx from '../../img/photo/company/zx.jpg';
-import imgPhotoYutong from '../../img/photo/company/Yutong.jpg';
-import imgPhotoSaic from '../../img/photo/company/saic.jpg';
 import imgPhotoChangan from '../../img/photo/company/Changan.jpg';
+import imgPhotoChery from '../../img/photo/company/chery.jpg';
+import imgPhotoDfm from '../../img/photo/company/dongfeng.jpg';
+import imgPhotoFaw from './../../img/photo/company/dongfeng.jpg';
+import imgPhotoGac from '../../img/photo/company/GAC Group.jpg';
+import imgPhotoGelly from '../../img/photo/company/geely.jpg';
+import imgPhotoGWM from '../../img/photo/company/gwm.jpg';
+import imgPhotoJac from '../../img/photo/company/jac.jpg';
 import imgPhotoLi from '../../img/photo/company/li.jpg';
+import imgPhotoSaic from '../../img/photo/company/saic.jpg';
+import imgPhotoYutong from '../../img/photo/company/Yutong.jpg';
+import imgPhotoZx from '../../img/photo/company/zx.jpg';
 
 const ImgPhotoCompanies = {
-    imgPhotoGelly,
-    imgPhotoChery,
-    imgPhotoJac,
-    imgPhotoGWM,
     imgPhotoBaic,
     imgPhotoByd,
+    imgPhotoChangan,
+    imgPhotoChery,
     imgPhotoDfm,
     imgPhotoFaw,
     imgPhotoGac,
-    imgPhotoZx,
-    imgPhotoYutong,
+    imgPhotoGelly,
+    imgPhotoGWM,
+    imgPhotoJac,
+    imgPhotoLi,
     imgPhotoSaic,
-    imgPhotoChangan,
-    imgPhotoLi
+    imgPhotoYutong,
+    imgPhotoZx
 }
  
 export default ImgPhotoCompanies;
